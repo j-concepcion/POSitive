215345c8bf8fc2c04d204dccafeb7bc52c3199e6
Janice Concepcion <concepcionja@gmail.com>
2014-08-29 06:53:48 +0800

    Iventorygit add -i (semifinished --edit for silog inventories).

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140827171122) do
+ActiveRecord::Schema.define(version: 20140828203744) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,8 +38,6 @@ ActiveRecord::Schema.define(version: 20140827171122) do
   create_table "inventories", force: true do |t|
     t.string   "item_name"
     t.integer  "quantity"
-    t.decimal  "buying_price"
-    t.decimal  "selling_price"
     t.date     "date_purchased"
     t.date     "expiration_date"
     t.string   "waste_age"
